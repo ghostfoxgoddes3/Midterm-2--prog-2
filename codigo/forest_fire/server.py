d59993a21ff8aa5e19cf9a1d5cc6642c881d950a
Maria Eduarda Martins <al.maria.martins@impatech.org.br>
2024-11-07 15:45:01 -0300

    Mudanças para rodar a visualização ao rodar server.py

--- codigo/forest_fire/server.py
+++ codigo/forest_fire/server.py
@@ -1,5 +1,4 @@
 import mesa
-
 from model import ForestFire
 
 COLORS = {"Fine": "#00AA00", "On Fire": "#880000", "Burned Out": "#000000"}
@@ -15,22 +14,32 @@ def forest_fire_portrayal(tree):
     portrayal["Color"] = COLORS[tree.condition]
     return portrayal
 
-
+# Definindo o Canvas para a visualização do modelo
 canvas_element = mesa.visualization.CanvasGrid(
     forest_fire_portrayal, 100, 100, 500, 500
 )
+
+# Gráfico de barras para as árvores
 tree_chart = mesa.visualization.ChartModule(
-    [{"Label": label, "Color": color} for (label, color) in COLORS.items()]
-)
-pie_chart = mesa.visualization.PieChartModule(
-    [{"Label": label, "Color": color} for (label, color) in COLORS.items()]
+    [{"Label": label, "Color": color} for label, color in COLORS.items()]
 )
 
+# Gráfico de pizza para a distribuição das árvores
+pie_chart = mesa.visualization.PieChartModule(
+    [{"Label": label, "Color": color} for label, color in COLORS.items()]
+)
+
+# Parâmetros do modelo (dimensões do grid e densidade das árvores)
 model_params = {
     "height": 100,
     "width": 100,
     "density": mesa.visualization.Slider("Tree density", 0.65, 0.01, 1.0, 0.01),
 }
+
+# Inicializando o servidor
 server = mesa.visualization.ModularServer(
     ForestFire, [canvas_element, tree_chart, pie_chart], "Forest Fire", model_params
 )
+
+server.port = 8521  # Porta do servidor
+server.launch()

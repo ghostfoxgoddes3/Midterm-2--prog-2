27970eb1be6b20a4162d1336cb5c775b9983a8e8
Laura Fernandes <167801527+laurafernandesp@users.noreply.github.com>
2024-11-12 17:01:51 -0300

    Update 2 - prob_de_sobrevivencia

--- codigo/forest_fire/model.py
+++ codigo/forest_fire/model.py
@@ -1,5 +1,4 @@
 import mesa
-
 from agent import TreeCell
 
 
@@ -8,18 +7,20 @@ class ForestFire(mesa.Model):
     Simple Forest Fire model.
     """
 
-    def __init__(self, width=100, height=100, density=0.65):
+    def __init__(self, width=100, height=100, density=0.65, prob_de_sobrevivencia=0.5):
         """
         Create a new forest fire model.
 
         Args:
             width, height: The size of the grid to model
             density: What fraction of grid cells have a tree in them.
+            prob_de_sobrevivencia: The probability that a tree survives fire.
         """
         super().__init__()
         # Set up model objects
         self.schedule = mesa.time.RandomActivation(self)
         self.grid = mesa.space.SingleGrid(width, height, torus=False)
+        self.prob_de_sobrevivencia = prob_de_sobrevivencia  # Adiciona a probabilidade de sobrevivência
 
         self.datacollector = mesa.DataCollector(
             {

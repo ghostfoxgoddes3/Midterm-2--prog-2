9385de5f1fd4133452c7bb56611159cf9b4311cd
Laura Fernandes <167801527+laurafernandesp@users.noreply.github.com>
2024-11-12 17:55:12 -0300

    Update 3 - server.py
    update 3

--- Testes/Laura/Probability of survival/server.py
+++ Testes/Laura/Probability of survival/server.py
@@ -1,1 +1,12 @@
+# Parâmetros do modelo (dimensões do grid, densidade das árvores e probabilidade de sobrevivência)
+model_params = {
+    "height": 100,
+    "width": 100,
+    "density": mesa.visualization.Slider("Tree density", 0.65, 0.01, 1.0, 0.01),
+    "prob_de_sobrevivencia": mesa.visualization.Slider("Probability of survival", 0.5, 0.0, 1.0, 0.01)
+}
 
+# Inicializando o servidor
+server = mesa.visualization.ModularServer(
+    ForestFire, [canvas_element, tree_chart, pie_chart], "Forest Fire", model_params
+)

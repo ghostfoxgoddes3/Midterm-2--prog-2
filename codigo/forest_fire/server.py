a61bf0d0cb96ec87eebe492df061701d22b365e0
Laura Fernandes <167801527+laurafernandesp@users.noreply.github.com>
2024-11-12 17:33:51 -0300

    Update 3 - prob_de_sobrevivencia

--- codigo/forest_fire/server.py
+++ codigo/forest_fire/server.py
@@ -29,17 +29,19 @@ pie_chart = mesa.visualization.PieChartModule(
     [{"Label": label, "Color": color} for label, color in COLORS.items()]
 )
 
-# Parâmetros do modelo (dimensões do grid e densidade das árvores)
+# Parâmetros do modelo (dimensões do grid, densidade das árvores e probabilidade de sobrevivência)
 model_params = {
     "height": 100,
     "width": 100,
     "density": mesa.visualization.Slider("Tree density", 0.65, 0.01, 1.0, 0.01),
+    "prob_de_sobrevivencia": mesa.visualization.Slider("Probability of survival", 0.5, 0.0, 1.0, 0.01)
 }
 
 # Inicializando o servidor
 server = mesa.visualization.ModularServer(
     ForestFire, [canvas_element, tree_chart, pie_chart], "Forest Fire", model_params
 )
+
 
 server.port = 8521  # Porta do servidor
 server.launch()

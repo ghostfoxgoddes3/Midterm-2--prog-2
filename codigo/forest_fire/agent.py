b944b2c7af7f4d45c61d983ed557bccb844631f3
Maria-Fernanda-IMPA <al.maria.cunha@impatech.org.br>
2024-11-16 12:35:30 -0300

    Update agent.py
    Adicionando a versão 2 do código ja com a probabilidade de sobrevivencia de arvore feito anterioemente pela Laura, e cidades evacuando

--- codigo/forest_fire/agent.py
+++ codigo/forest_fire/agent.py
@@ -1,7 +1,13 @@
-from mesa import Agent
+import mesa
+import math
 
-class TreeCell(Agent):
-    def __init__(self, pos, model, prob_de_sobrevivencia=0.5):
+
+class TreeCell(mesa.Agent):
+    """
+    A tree in the forest.
+    """
+
+    def __init__(self, pos, model, prob_de_sobrevivencia):
         super().__init__(pos, model)
         self.pos = pos
         self.model = model
@@ -30,3 +36,46 @@ class TreeCell(Agent):
             self.condition = "Burned Out"
         else:
             print(f"Tree at {self.pos} is in state: {self.condition}")
+
+    def in_bounds(self, pos): #ADICIONAR COMENTARIO DO PQ DESSA FUNÇÃO
+        x, y = pos
+        # Verificar se as coordenadas estão dentro dos limites da grade
+        return 0 <= x < self.model.grid.width and 0 <= y < self.model.grid.height
+
+
+
+
+class CityCell(mesa.Agent):
+    """
+    A city in the forest.
+    """
+
+    def __init__(self, pos, model, condition = "City"):
+        super().__init__(pos, model)
+        self.pos = pos
+        self.condition = "City"  # Condições possíveis: "City", "Evacuated"
+        self.alert = False  # Flag para indicar o alerta de evacuação
+
+    def step(self):
+        # Alerta de evacuação para as cidades dentro do raio de 10 células
+        if self.condition == "City":
+            for x in range(self.pos[0] - 10, self.pos[0] + 11):
+                for y in range(self.pos[1] - 10, self.pos[1] + 11):
+                    # Ignorar a célula da cidade ou fora dos limites
+                    if (x, y) == self.pos or not (0 <= x < self.model.grid.width and 0 <= y < self.model.grid.height):
+                        continue
+                    # Calcular a distância entre as células
+                    distance = math.sqrt((self.pos[0] - x) ** 2 + (self.pos[1] - y) ** 2)
+                    # Se o fogo está dentro do raio e encontrou uma célula em chamas, evacuar
+                    if distance <= 10:
+                        neighbor = self.model.grid.get_cell_list_contents([(x, y)])
+                        for n in neighbor:
+                            if n.condition == "On Fire":
+                                self.alert = True
+                                self.condition = "Evacuated"  # Evacuar a cidade
+                                return
+
+    def in_bounds(self, pos):
+        x, y = pos
+        # Verificar se as coordenadas estão dentro dos limites da grade
+        return 0 <= x < self.model.grid.width and 0 <= y < self.model.grid.height

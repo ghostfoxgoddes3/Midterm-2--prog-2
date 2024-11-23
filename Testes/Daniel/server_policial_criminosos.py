d7a28738d4bb276ea7783491f352e7ed115ba036
danielcoutop <167899707+danielcoutop@users.noreply.github.com>
2024-11-23 17:50:51 -0300

    Update and rename server_policia_incendiario.py to server_policial_criminosos.py

--- Testes/Daniel/server_policial_criminosos.py
+++ Testes/Daniel/server_policial_criminosos.py
@@ -1,5 +1,5 @@
 import mesa
-from model import ForestFire, TreeCell, Police, Incendiary
+from model import ForestFire, TreeCell, Police, Bomber, Logger
 
 # Cores para os diferentes estados e agentes
 COLORS = {
@@ -8,12 +8,15 @@ COLORS = {
     "Burned Out": "#d4d6d5",  # Cor para árvores queimadas
     "Fire Off": "#6495ED",  # Cor para árvore apagada pelo bombeiro
     "Police": "#0000FF",  # Cor do policial
-    "Incendiary": "#FF4500",  # Cor do incendiário
+    "Bomber": "#FF0000",  # Cor do Bombardeiro
+    "Toasted": "#000000", #Cor da Árvore bombardeada
+    "Logger": "#8B4513",  # Cor do Logger (Madeireiro)
+    "Blank": "#FFFFFF"
 }
 
 def forest_fire_portrayal(agent):
     """
-    Função para exibir visualmente as árvores policiais e incendiários.
+    Função para exibir visualmente as árvores policiais e Bombardeiros.
     """
     if agent is None:
         return None
@@ -28,19 +31,47 @@ def forest_fire_portrayal(agent):
             "Filled": "true",
             "Color": COLORS["Police"],
             "Layer": 3,
-            "r": 1.5,  # Raio do círculo
+            "r": 2,  # Raio do círculo
         }
 
-    # Incendiário
-    elif isinstance(agent, Incendiary):
-        portrayal = {
-            "Shape": "circle",
-            "Filled": "true",
-            "Color": COLORS["Incendiary"],
-            "Layer": 3,
-            "r": 1.5,
-        }
-
+    # Bombardeiro
+    elif isinstance(agent, Bomber):
+        # Verifica se o Bombardeiro foi capturado e altera a cor para Roxo
+        if agent.captured:
+            portrayal = {
+                "Shape": "circle",
+                "Filled": "true",
+                "Color": "#A020F0",  # Roxo
+                "Layer": 3,
+                "r": 2,
+            }
+        else:
+            portrayal = {
+                "Shape": "circle",
+                "Filled": "true",
+                "Color": COLORS["Bomber"],
+                "Layer": 3,
+                "r": 2,
+            }
+     # Logger (Madeireiro)
+    elif isinstance(agent, Logger):
+        # Verifica se o Madeireiro foi capturado e altera a cor para Roxo
+        if agent.captured:
+            portrayal = {
+                "Shape": "circle",
+                "Filled": "true",
+                "Color": "#A020F0", 
+                "Layer": 3,
+                "r": 2,
+            }
+        else:
+            portrayal = {
+                "Shape": "circle",
+                "Filled": "true",
+                "Color": COLORS["Logger"],
+                "Layer": 3,
+                "r": 2,
+            }
     # Árvore
     elif isinstance(agent, TreeCell):
         portrayal = {
@@ -91,7 +122,8 @@ model_params = {
     "density": mesa.visualization.Slider("Tree density", 0.65, 0.01, 1.0, 0.01),
     "prob_de_sobrevivencia": mesa.visualization.Slider("Probability of survival", 0.5, 0.0, 1.0, 0.01),
     "num_policiais": mesa.visualization.Slider("Number of police officers", 5, 0, 20, 1),
-    "num_incendiarios": mesa.visualization.Slider("Number of incendiaries", 3, 0, 20, 1),
+    "num_bombers": mesa.visualization.Slider("Number of Bombers", 3, 0, 20, 1),
+    "num_loggers": mesa.visualization.Slider("Number of loggers", 2, 0, 20, 1),
 }
 
 # Inicializando o servidor
